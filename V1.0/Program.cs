23376f5c2dcefa44174fe7c504ac5613000f9a94
Nicolae Gorea <nicugorea99@gmail.com>
2025-02-17 21:36:06 +0200

    V1

---
+++ V1.0/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Net.Http.Json;
+
+while (true)
+{
+    using var httpClient = new HttpClient();
+    using var response = await httpClient.GetAsync(args[0]);
+    var deserializedResponse = await response.Content.ReadFromJsonAsync<dynamic>();
+    Console.WriteLine($"Current exchange rate is: {deserializedResponse?.Value}.");
+    await Task.Delay(TimeSpan.FromMinutes(5));
+}
---
+++ V1.1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Net.Http.Json;
+
+
+if (!Uri.TryCreate(args[0], UriKind.RelativeOrAbsolute, out var uri))
+{
+    Console.Error.WriteLine("The API endpoint URL is not valid.");
+    return;
+}
+
+while (true)
+{
+    using var httpClient = new HttpClient();
+    using var response = await httpClient.GetAsync(uri);
+    if (!response.IsSuccessStatusCode)
+    {
+        Console.Error.WriteLine($"The response status code ({response.StatusCode}) does not indicate success.");
+        continue;
+    }
+
+    try
+    {
+        var deserializedResponse = await response.Content.ReadFromJsonAsync<dynamic>();
+        Console.Out.WriteLine($"Current exchange rate is: {deserializedResponse?.Value}.");
+    }
+    catch (Exception exception)
+    {
+        Console.Error.WriteLine($"An error occured while deserializing the response content: {exception}");
+        continue;
+    }
+
+    await Task.Delay(TimeSpan.FromMinutes(5));
+}
